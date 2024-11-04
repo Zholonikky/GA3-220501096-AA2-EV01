26c3d76c55ab0b3fe3c9fa32a12fa66807ab8ab7
Zholonikky <remolina.92@hotmail.com>
2024-11-04 13:59:35 -0500

    Evidencia 3

---
+++ script.js
@@ -0,0 +1,9 @@
+let hora = prompt("Ingrese la hora por favor");
+
+if (hora>=6 && hora<=12){
+    alert("Buenos dias!");
+}else if(hora>=13 && hora<=18){
+    alert("Buenas tardes!");
+}else{
+    alert("Buenas noches!");
+}
